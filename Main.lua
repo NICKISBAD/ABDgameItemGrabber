0ee2750d0fe6dc22aac9023239b219f1cd4e97ce
Nick <107813557+NICKISBAD@users.noreply.github.com>
2024-01-08 18:41:15 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,30 +1,30 @@
 -- Functions
 function GrabItems()
-    for i,obj in pairs(game.Workspace:GetDescendants()) do
-        if obj:IsA("Tool") then
-            local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
-            if primarypart.CFrame then
-                primarypart.CFrame = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
-            elseif not primarypart.CFrame and primarypart.Position then
-                primarypart.Position = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position
+	for i,obj in pairs(game.Workspace:GetDescendants()) do
+		if obj:IsA("Tool") then
+			local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
+			if primarypart.CFrame then
+				primarypart.CFrame = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
+			elseif not primarypart.CFrame and primarypart.Position then
+				primarypart.Position = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position
             end
         end
     end
 end
 
 function TPtoItems()
-    for i,obj in pairs(game.Workspace:GetDescendants()) do
-        if obj:IsA("Tool") then
-            local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
-            if primarypart.CFrame and not primarypart.Position then
-                game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = primarypart.CFrame
-                wait(.5)
-            elseif not primarypart.CFrame and primarypart.Position then
-                game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position = primarypart.Position
-                wait(.5)
-            end
-        end
-    end
+	for i,obj in pairs(game.Workspace:GetDescendants()) do
+		if obj:IsA("Tool") then
+			local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
+			if primarypart.CFrame and not primarypart.Position then
+				game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = primarypart.CFrame
+				wait(.5)
+			elseif not primarypart.CFrame and primarypart.Position then
+				game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position = primarypart.Position
+				wait(.5)
+			end
+		end
+	end
 end
 
 -- Tables
@@ -47,22 +47,22 @@ Section1:NewToggle("Item Grabber", "Grabs all items in the map", function(value)
 end)
 
 spawn(function()
-    game.RunService.RenderStepped:Connect(function()
-        if ItemGrabbing and not BlacklistApplied then
-          GrabItems()
-        elseif ItemGrabbing and BlacklistApplied then
-          for i,obj in pairs(game.Workspace:GetDescendants()) do
-            if obj:IsA("Tool") and not table.find(IgnoredItems, obj.Name) then
-              local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
-              if primarypart.CFrame then
-                primarypart.CFrame = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
-            elseif not primarypart.CFrame and primarypart.Position then
-                primarypart.Position = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position
-          end
-        end
-      end
-    end
-  end)
+	game.RunService.RenderStepped:Connect(function()
+		if ItemGrabbing and not BlacklistApplied then
+			GrabItems()
+		elseif ItemGrabbing and BlacklistApplied then
+			for i,obj in pairs(game.Workspace:GetDescendants()) do
+				if obj:IsA("Tool") and not table.find(IgnoredItems, obj.Name) then
+					local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
+					if primarypart.CFrame then
+						primarypart.CFrame = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
+					elseif not primarypart.CFrame and primarypart.Position then
+						primarypart.Position = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position
+					end
+				end
+			end
+		end
+	end)
 end)
 
 local Section2 = Tab:NewSection("Item TPer")
@@ -72,22 +72,22 @@ Section2:NewToggle("Item TPer", "Tps to all items in the map", function(value)
 end)
 
 spawn(function()
-    game.RunService.RenderStepped:Connect(function()
-        if ItemTPing and not BlacklistApplied then
-          TptoItems()
-        elseif ItemTPing and BlacklistApplied then
-          for i,obj in pairs(game.Workspace:GetDescendants()) do
-            if obj:IsA("Tool") and not table.find(IgnoredItems, obj.Name) then
-              local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
-              if primarypart.CFrame then
-                game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = primarypart.CFrame
-            elseif not primarypart.CFrame and primarypart.Position then
-                game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position = primarypart.Position
-          end
-        end
-      end
-    end
-  end)
+	game.RunService.RenderStepped:Connect(function()
+		if ItemTPing and not BlacklistApplied then
+			TptoItems()
+		elseif ItemTPing and BlacklistApplied then
+			for i,obj in pairs(game.Workspace:GetDescendants()) do
+				if obj:IsA("Tool") and not table.find(IgnoredItems, obj.Name) then
+					local primarypart = obj:FindFirstChild("Cover") or obj:FindFirstChildWhichIsA("BasePart") or obj:FindFirstChildWhichIsA("Part") or obj:FindFirstChildWhichIsA("MeshPart")
+					if primarypart.CFrame then
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = primarypart.CFrame
+					elseif not primarypart.CFrame and primarypart.Position then
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position = primarypart.Position
+					end
+				end
+			end
+		end
+	end)
 end)
 
 local Section3 = Tab:NewSection("--Settings--")
@@ -97,26 +97,26 @@ Section3:NewTextBox("Blacklist Items", "Adds an Item to blacklist for itemtping/
 end)
 
 Section3:NewTextBox("Remove Blacklist Items", "Removes an Item to blacklist for itemtping/itemgrabbing", function(value)
-		if IgnoredItems[value] then
-			table.remove(IgnoredItems, value)
-		elseif not IgnoredItems[value] then
-			game.StarterGui:SetCore("SendNotification",{
-				Title = "Error",
-				Text = value .. " was not found in blacklisted items!",
-				Duration = 5
-			})
-		end
+	if IgnoredItems[value] then
+		table.remove(IgnoredItems, value)
+	elseif not IgnoredItems[value] then
+		game.StarterGui:SetCore("SendNotification",{
+			Title = "Error",
+			Text = value .. " was not found in blacklisted items!",
+			Duration = 5
+		})
+	end
 end)
 
 local BlacklistedItemsLabel = Section3:NewLabel("BlacklistedItems: ")
 
 spawn(function()
 	while wait() do
-			for _,v in ipairs(IgnoredItems) do
-				BlacklistedItemsLabel:UpdateLabel("BlacklistedItems: " .. v)
+		for _,v in ipairs(IgnoredItems) do
+			BlacklistedItemsLabel:UpdateLabel("BlacklistedItems: " .. v)
 		end
 	end
-end
+end)
 
 Section3:NewToggle("Blacklisting Active", "Activates/Deactivates item blacklist", function(v)
 	IgnoredItems = v
